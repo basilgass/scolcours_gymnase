14d7fe2c577eae4a1fdd4908ed60a98cd4ae297b
basilgass <basil.gass@bluewin.ch>
2022-03-06 22:12:15 +0100

    Updating pi.js and adding some new tools. WIP on the future post/exercise/chapter systems...

--- app/Http/Controllers/LatexController.php
+++ app/Http/Controllers/LatexController.php
@@ -1,27 +1,25 @@
 <?php
-	
+
 	namespace App\Http\Controllers;
-	
+
 	use App\Models\LatexPdf;
 	use Illuminate\Http\Request;
-	use Illuminate\Support\Facades\File;
 	use Illuminate\Support\Facades\Storage;
 	use Illuminate\Support\Str;
 	use Ismaelw\LaraTeX\LaraTeX;
-	use Symfony\Component\Process\Process;
-	
+
 	class LatexController extends Controller
 	{
 		public function latex(Request $data) // POST request
 		{
 			$requestData = json_decode($data->getContent());
-			
+
 			// Check if folder exists.
 			$folder = "pdf/{$requestData->theme}/{$requestData->slug}";
 			if (!Storage::disk('public')->exists($folder)) {
 				Storage::disk('public')->makeDirectory($folder);
 			}
-			
+
 			// Generate a filename
 			// TODO: Generate slug of 4 letters
 			$fileID = Str::random(4);
@@ -29,17 +27,17 @@
 			while (LatexPdf::whereSlug($fileID)->first()) {
 				$fileID = Str::random(4);
 			}
-			
+
 			$filename = $fileID . '.pdf';
 			$fullpath = $folder . '/' . $filename;
-			
+
 			$result = (new LaraTeX('latex.WhatToKnow'))->with([
 				                                                  'title'     => $requestData->title,
 				                                                  'questions' => $requestData->questions,
 				                                                  'slug'      => '/download/' . $fileID
 			                                                  ])
 			                                           ->savePdf(Storage::disk('public')->path($fullpath));
-			
+
 			if ($result) {
 				// Save it to the database.
 				LatexPdf::create([
@@ -50,11 +48,11 @@
 			}
 			return $fileID;
 		}
-		
+
 		public function download(string $fileID) // Download file name from public/pdf
 		{
 			$pdf = LatexPdf::whereSlug($fileID)->first();
-			
+
 			if ($pdf) {
 				return Storage::disk('public')
 				              ->download(
@@ -62,70 +60,77 @@
 					              $pdf->name
 				              );
 			}
-			
+
 			return false;
-			
+
 		}
-		
-		public function toPng()
+
+		public function dry()
 		{
-			$outputFile = false;
-			$latexBinFolder = dirname(config('laratex.binPath'));
-			
-			$content = "Hello world";
-			$result = (new LaraTeX('latex.standalone'))->with([
-				                                                  'standalone' => true,
-				                                                  'content'    => $content
-			                                                  ])
-			                                           ->render();
-			
-			// Save the file
-			$compileFolder = storage_path(config('laratex.tempPath'));
-			File::put($compileFolder . '/temp.tex', $result);
-			
-			$convertOptions = [
-				$latexBinFolder . '/latex',
-				'temp.tex'
-			];
-			
-			$home = "C:/websites/scolcours_gymnase/";
-			if(env('APP_ENV')==='local'){
-				$localEnv = [
-					'HOME'        => $home,
-					'HOMEPATH'    => $home,
-					'HOMEDRIVE'   => $home,
-					'USERPROFILE' => $home,
-				];
-			}else{
-				$localEnv = null;
-			}
-			
-			$convert = new Process($convertOptions, $compileFolder, $localEnv);
-			$convert->run();
-			
-			if (!$convert->isSuccessful()) {
-				dd('FAILED COMPILE TO DVI', $convertOptions,
-				   $convert->getOutput(),
-				   $convert->getErrorOutput());
-			}
-			
-			
-			// Convert to png
-			$convertOptions = [
-				$latexBinFolder . '/dvipng',
-				'temp'
-			];
-			
-			$convert = new Process($convertOptions, $compileFolder,
-			                       $localEnv);
-			
-			$convert->run();
-			
-			
-			if (!$convert->isSuccessful()) {
-				dd('FAILED CONVERT TO PNG', $convertOptions, $convert->getOutput(),$convert->getErrorOutput() );
-			}
-			
-			return $compileFolder . '/temp.png';
+			return (new LaraTeX)->dryRun();
+
 		}
+//		public function toPng()
+//		{
+//			$outputFile = false;
+//			$latexBinFolder = dirname(config('laratex.binPath'));
+//
+//			$content = "Hello world";
+//			$result = (new LaraTeX('latex.standalone'))->with([
+//				                                                  'standalone' => true,
+//				                                                  'content'    => $content
+//			                                                  ])
+//			                                           ->render();
+//
+//			// Save the file
+//			$compileFolder = storage_path(config('laratex.tempPath'));
+//			File::put($compileFolder . '/temp.tex', $result);
+//
+//			$convertOptions = [
+//				$latexBinFolder . '/latex',
+//				'temp.tex'
+//			];
+//
+//			$home = "C:/websites/scolcours_gymnase/";
+//			if(env('APP_ENV')==='local'){
+//				$localEnv = [
+//					'HOME'        => $home,
+//					'HOMEPATH'    => $home,
+//					'HOMEDRIVE'   => $home,
+//					'USERPROFILE' => $home,
+//				];
+//			}else{
+//				$localEnv = null;
+//			}
+//
+//			$convert = new Process($convertOptions, $compileFolder, $localEnv);
+//			$convert->run();
+//
+//			if (!$convert->isSuccessful()) {
+//				dd('FAILED COMPILE TO DVI', $convertOptions,
+//				   $convert->getOutput(),
+//				   $convert->getErrorOutput());
+//			}
+//
+//
+//			// Convert to png
+//			$convertOptions = [
+//				$latexBinFolder . '/dvipng',
+//				'temp'
+//			];
+//
+//			$convert = new Process($convertOptions, $compileFolder,
+//			                       $localEnv);
+//
+//			$convert->run();
+//
+//
+//			if (!$convert->isSuccessful()) {
+//				dd('FAILED CONVERT TO PNG', $convertOptions, $convert->getOutput(),$convert->getErrorOutput() );
+//			}
+//
+//			return $compileFolder . '/temp.png';
+//		}
+
+
 	}
--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,26 @@
 <?php
-	
-	use App\Http\Controllers\AdminController;
-	use App\Http\Controllers\ChallengesController;
-	use App\Http\Controllers\ChaptersController;
-	use App\Http\Controllers\LatexController;
-	use App\Http\Controllers\PostController;
-	use App\Http\Controllers\ToolsController;
-	use App\Models\Theme;
-	use Illuminate\Support\Facades\Route;
-	use Inertia\Inertia;
-	
-	/*
-	|--------------------------------------------------------------------------
-	| Web Routes
-	|--------------------------------------------------------------------------
-	|
-	| Here is where you can register web routes for your application. These
-	| routes are loaded by the RouteServiceProvider within a group which
-	| contains the "web" middleware group. Now create something great!
-	|
-	*/
-	
+
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ChallengesController;
+use App\Http\Controllers\ChaptersController;
+use App\Http\Controllers\LatexController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\ToolsController;
+use App\Models\Theme;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
 	// Home page - no controller
 	Route::get('/', function () {
 		$themes = Theme::all();
@@ -47,24 +47,25 @@
 	require __DIR__ . '/auth.php';
 
 // Latex download - migrate to custom controller
+Route:get('/latex/dry', [LatexController::class, 'dry']);
 	Route::post('/latex', [LatexController::class, 'latex']);
 	Route::get('/download/{fileID}', [LatexController::class, 'download']);
-	
+
 	// Developpement page
 	Route::get('dev/', function () {
 		return Inertia::render("Dev");
 	});
-	
+
 	// Post routes (for debug / dev)
 	Route::post('post/', [PostController::class, 'store']);
 	Route::get('post/create', [PostController::class, 'create']);
 	Route::get('post/{post}', [PostController::class, 'show']);
-	
+
 	// Tools controller
 	Route::get('tools/', [ToolsController::class, 'index'])->name('tools');
 	Route::get('tools/{tool:slug}', [ToolsController::class, 'show'])->name('tools.tool');
-	
+
 	// Chapter controller
 	Route::get('{theme:slug}/', [ChaptersController::class, 'index'])->name('theme');
 	Route::get('{theme:slug}/{chapter:slug}', [ChaptersController::class, 'show'])->name('theme.chapter');
-		
+
