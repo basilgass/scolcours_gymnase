725e7256b9ca7cd5e73626775ea9be2d3e1d5249
basilgass <basil.gass@bluewin.ch>
2022-03-24 14:14:13 +0100

    Moving to module

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,29 +1,29 @@
-import { createApp, h } from 'vue'
-import { createInertiaApp, Head, Link } from '@inertiajs/inertia-vue3'
-import { InertiaProgress } from '@inertiajs/progress'
+import {createApp, h} from "vue"
+import {createInertiaApp, Head, Link} from "@inertiajs/inertia-vue3"
+import {InertiaProgress} from "@inertiajs/progress"
 // Custom directives
-import { katexDirective, visibleDirective } from '@/vueDirectives'
+import {katexDirective, visibleDirective} from "@/vueDirectives"
 
-require('./bootstrap')
+require("./bootstrap")
 
-const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'ScolCours'
+const appName = window.document.getElementsByTagName("title")[0]?.innerText || "ScolCours"
 createInertiaApp({
 	title: (title) => `${title} - ${appName}`,
 	resolve: name => import(`./Pages/${name}`),
-	setup ({ el, app, props, plugin }) {
+	setup({el, app, props, plugin}) {
 		const vueApp = createApp(
-			{ render: () => h(app, props) })
-			.component('Link', Link)
-			.component('Head', Head)
+			{render: () => h(app, props)})
+			.component("Link", Link)
+			.component("Head", Head)
 			.use(plugin)
-			.directive('katex', katexDirective)
-			.directive('visible', visibleDirective)
-			.mixin({ methods: { route } })
-		
+			.directive("katex", katexDirective)
+			.directive("visible", visibleDirective)
+			.mixin({methods: {route}})
+
 		vueApp.config.globalProperties.$log = console.log
-		
+
 		return vueApp.mount(el)
 	}
 })
 
-InertiaProgress.init({ color: '#4B5563' })
+InertiaProgress.init({color: "#4B5563"})
