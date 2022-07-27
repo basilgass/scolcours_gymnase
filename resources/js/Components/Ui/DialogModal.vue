986b70b1a2699ecfd3bc2a87054e22cddbce410a
basilgass <basil.gass@bluewin.ch>
2022-07-27 11:45:13 +0200

    Mise à jour, une partie de design Mise à jour de PiDraw

--- resources/js/Components/Ui/DialogModal.vue
+++ resources/js/Components/Ui/DialogModal.vue
@@ -1,25 +1,32 @@
 <template>
-	<dialog
-		ref="root"
-		class="rounded-lg"
+	<div
+		v-if="props.modelValue"
+		class="fixed inset-0 bg-gray-800/60 grid place-items-center"
 		@click.self="doCancel"
-		@close="emits('update:modelValue', false)"
 	>
-		<div v-if="$slots.header">
-			<slot name="header" />
+		<div
+			ref="root"
+			class="grid items-center bg-white rounded-lg px-5 py-3 max-w-[800px] w-[80%]"
+			@close="emits('update:modelValue', false)"
+		>
+			<div>
+				<div v-if="$slots.header">
+					<slot name="header" />
+				</div>
+				<div>
+					<slot />
+				</div>
+				<div v-if="$slots.footer">
+					<slot name="footer" />
+				</div>
+			</div>
 		</div>
-		<div>
-			<slot />
-		</div>
-		<div v-if="$slots.footer">
-			<slot name="footer" />
-		</div>
-	</dialog>
+	</div>
 </template>
 
 <script setup>
 
-import {ref, watch} from "vue"
+import {ref} from "vue"
 
 const emits = defineEmits([
 	"update:modelValue", "cancel"
@@ -30,9 +37,9 @@ const  props = defineProps({
 })
 
 // Detect the change of the modelValue.
-watch(()=>props.modelValue, (val)=>{
-	if(val){root.value.showModal()}else{root.value.close()}
-})
+// watch(()=>props.modelValue, (val)=>{
+// 	if(val){root.value.showModal()}else{root.value.close()}
+// })
 
 let root = ref(null)
 
