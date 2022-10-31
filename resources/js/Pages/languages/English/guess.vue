3bfb629c9c7cadc27b3f975f983c6d0fb6d85c71
basilgass <basil.gass@bluewin.ch>
2022-10-31 20:31:42 +0100

    Updating the English suggestion system TODO : make it modulable for italian and english

--- resources/js/Pages/languages/English/guess.vue
+++ resources/js/Pages/languages/English/guess.vue
@@ -39,17 +39,24 @@
 			</div>
 
 			<div
-				v-if="suggestions.length>0"
+				v-if="suggestionsItems.length>0"
 				ref="suggestionsWrapper"
 				class="suggestions flex flex-col bg-white border rounded my-4 divide-y"
 			>
 				<div
-					v-for="(word, index) in suggestions"
+					v-for="(word, index) in suggestionsItems"
 					:key="index"
-					class="px-4 py-3 hover:bg-amber-100 transition-all duration-300 cursor-pointer"
-					@click="suggestionClick(word)"
+					class="px-4 py-3 hover:bg-amber-100 transition-all duration-300 cursor-pointer flex justify-between"
+					@click="suggestionClick(index)"
 				>
-					{{ word[0] }}
+					<div>
+						{{ word.foreign }}
+					</div>
+					<transition name="fade-right">
+						<div v-show="word.hint">
+							{{ word.fr }}
+						</div>
+					</transition>
 				</div>
 			</div>
 
@@ -88,7 +95,7 @@
 				label="nombre de lettres min."
 				name="nombre de lettres"
 			/>
-			
+
 			<form-input
 				v-model="unitsSelection"
 				label="sélection des unités"
@@ -107,7 +114,7 @@ export default {
 </script>
 <script setup>
 import ArticleTitle from "@/Components/Ui/ArticleTitle.vue"
-import {computed, nextTick, ref} from "vue"
+import {computed, nextTick, ref, watch} from "vue"
 import {PiMath} from "pimath/esm"
 import FormInput from "@/Components/Form/FormInput.vue"
 import FormNumber from "@/Components/Form/FormNumber.vue"
@@ -118,24 +125,8 @@ import FormSwitch from "@/Components/Form/FormSwitch.vue"
 let availableWords = ref([]),
 	numberOfLetters = ref(2),
 	userGuess = ref(""),
-	determinants = [],
-	suggestions = computed(()=>{
-		if(userGuess.value.length<numberOfLetters.value){return []}
-
-		const txt = userGuess.value.toLowerCase()
-
-		return availableWords.value.filter(x=> {
-			// C'est un rajout qui vient de "Je ne sais pas".
-			if(x.length===3){return false}
-			for(let det of determinants){
-				if(x[0].toLowerCase().startsWith(`${det}${txt}`)){
-					return true
-				}
-			}
-
-			return x[0].toLowerCase().startsWith(txt)
-		})
-	}),
+	determinants = ["a ", "to ", "the ", "an "],
+	suggestionsItems = ref([]),
 	suggestionsWrapper = ref(null),
 	startIndex = ref(0),
 	unitsSelection = ref("1"),
@@ -194,21 +185,26 @@ let generateWords = function() {
 }
 
 let suggestionEnter = function (){
-	if(suggestions.value.length===1){
-		suggestionClick(suggestions.value[0])
+	if(suggestionsItems.value.length===1){
+		suggestionClick(0)
 	}
 }
-let suggestionClick = function(word) {
-	if(word[0] === availableWords.value[startIndex.value][0]){
+let suggestionClick = function(index) {
+	if(suggestionsItems.value[index].foreign === availableWords.value[startIndex.value][0]){
 		// Continue the game
 		continueGame()
 	}else{
-		shake()
+		shake(index)
+
+		// Add the translation for one second.
+		suggestionsItems.value[index].hint = true
+		setTimeout(()=>suggestionsItems.value[index].hint = false, 2000)
 	}
 }
 
 let unknownWord = function () {
 	let word = availableWords.value[startIndex.value]
+
 	if(word.length===2){
 		availableWords.value.push([...word, 1])
 	}else{
@@ -219,13 +215,50 @@ let unknownWord = function () {
 	unknownCount.value++
 }
 
-function shake() {
-	suggestionsWrapper.value.style.setProperty("animation-name", "v-shake-horizontal")
-	suggestionsWrapper.value.style.setProperty("animation-duration", "500ms")
+function shake(index) {
+	let item = suggestionsWrapper.value.children[index]
+
+	item.style.setProperty("animation-name", "v-shake-horizontal")
+	item.style.setProperty("animation-duration", "500ms")
 
 	setTimeout(() => {
-		suggestionsWrapper.value.style.setProperty("animation-name", "")
+		item.style.setProperty("animation-name", "")
 	}, 500)
-
 }
+
+watch(userGuess, (newValue, oldValue)=>{
+	// if(userGuess.value.length<numberOfLetters.value){
+	// 	return []
+	// }
+
+	const txt = userGuess.value.toLowerCase()
+
+	suggestionsItems.value = availableWords.value.filter(x=> {
+		// C'est un rajout qui vient de "Je ne sais pas".
+		if(x.length===3){return false}
+
+		let translation = x[0].toLowerCase()
+		// On filtre les déterminants
+		for(let det of determinants){
+			if(translation.startsWith(det)){
+				translation = translation.substring(det.length)
+				break
+			}
+		}
+
+		// Le mot cherché a moins de lettre que le nombre de lettres actuelles.
+		if(txt === translation){
+			return true
+		}
+
+		// On retourne uniquement s'il commence
+		return txt.length>=numberOfLetters.value && translation.startsWith(txt)
+	}).map(x=>{
+		return {
+			foreign: x[0],
+			fr: x[1],
+			hint: false
+		}
+	})
+})
 </script>
