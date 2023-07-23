39a2052c36f4f6b0cb9b00eff9869d00a641d478
Basil <basil.gass@bluewin.ch>
2023-07-23 17:33:47 +0200

    Mise à jour des quizz.

---
+++ resources/js/Components/Devs/DevUi.vue
@@ -0,0 +1,30 @@
+<script setup>
+import UiSlider from "@/Components/Ui/UiSlider.vue"
+import {ref} from "vue"
+import FormNumber from "@/Components/Form/FormNumber.vue"
+
+
+let value = ref(15),
+	vmin = ref(10),
+	vmax = ref(40)
+</script>
+
+<template>
+	<form-number
+		v-model="vmin"
+		name="vmin"
+	/>
+	<form-number
+		v-model="vmax"
+		name="vmin"
+	/>
+	<div>
+		<ui-slider
+			v-model="value"
+			:max="vmax"
+			:min="vmin"
+		/>
+	</div>
+
+	<div>FROM UI COMPONENT: {{ value }}</div>
+</template>
