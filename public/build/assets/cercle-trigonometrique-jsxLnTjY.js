d331a05830de1e08af50687b3596d34979b2da65
Basil Gass <basil.gass@bluewin.ch>
2024-11-08 11:35:50 +0100

    Build

---
+++ public/build/assets/cercle-trigonometrique-jsxLnTjY.js
@@ -0,0 +1,56 @@
+import{_ as i}from"./PiDrawParser.vue_vue_type_script_setup_true_lang-BoZcjPj6.js";import{d as l,aU as r,a1 as t,a9 as c}from"./@vue-D-HBrq3Q.js";import"./@vueuse-Ds0mpvNY.js";import"./katex-BGZWzji8.js";import"./call-bind-aBC2DkHY.js";import"./get-intrinsic-BKEvijrG.js";import"./es-errors-DzOT6E3C.js";import"./has-symbols-eVqrYdw7.js";import"./has-proto-JnoBQRdH.js";import"./function-bind-BbkWVFrm.js";import"./hasown-DYqjtFKE.js";import"./set-function-length-B5OANX0j.js";import"./define-data-property-DO9o5wXF.js";import"./es-define-property-tzmkNPou.js";import"./gopd-CEkvUycD.js";import"./has-property-descriptors-DphFXkuo.js";import"./pidraw-ZGac3O9i.js";import"./pimath-qcbX-Z2q.js";import"./vue-slider-component-2NT_4acC.js";import"./vue-CiKT_iD6.js";const a={class:"bg-white max-w-3xl mx-auto"},e=`@begin:static
+A(0,0)
+c=circ A,10
+P0(10,0)
+P1(8.67,5)->tex=\\frac{\\pi}{6}/mr/0.5;0,fill=black,o=4
+P2(7.07,7.07)->tex=\\frac{\\pi}{4}/mr/0.5;0.4,fill=black,o=4
+P3(5,8.67)->tex=\\frac{\\pi}{3}/tr/0.5;0,fill=black,o=4
+P4(0,10)->tex=\\frac{\\pi}{2}/tl/-0.5;0.2,fill=black,o=4
+P7(-8.67,5)->tex=\\frac{5\\pi}{6}/ml/-0.5;0,fill=black,o=4
+P6(-7.07,7.07)->tex=\\frac{3\\pi}{4}/ml/-0.5;0.4,fill=black,o=4
+P5(-5,8.67)->tex=\\frac{2\\pi}{3}/tl/-0.5;0,fill=black,o=4
+P8(-10,0)->tex=\\frac{3\\pi}{2}/tl/-0.5;0.3,fill=black,o=4
+P9(8.67,-5)->tex=\\frac{11\\pi}{6}/mr/0.5;0,fill=black,o=4
+P10(7.07,-7.07)->tex=\\frac{7\\pi}{4}/br/0.5;0,fill=black,o=4
+P311(5,-8.67)->tex=\\frac{5\\pi}{3}/br/0.5;0,fill=black,o=4
+P12(0,-10)->tex=\\frac{3\\pi}{2}/bl/-0.5;-0.3,fill=black,o=4
+P13(-8.67,-5)->tex=\\frac{7\\pi}{6}/ml/-0.5;0,fill=black,o=4
+P14(-7.07,-7.07)->tex=\\frac{5\\pi}{4}/bl/-0.5;0,fill=black,o=4
+P15(-5,-8.67)->tex=\\frac{4\\pi}{3}/bl/-0.5;0,fill=black,o=4
+T1(5,0)->tex=\\scriptsize\\frac{1}{2}/cb/0;-0.5,fill=black,o=4
+T2(7.07,0)->tex=\\scriptsize\\frac{\\sqrt{2}}{2}/cb/0;-0.5,fill=black,o=4
+T3(8.6,0)->tex=\\scriptsize\\frac{\\sqrt{3}}{2}/cb/0;-0.5,fill=black,o=4
+T4(10,0)->tex=\\scriptsize 1/rb/0.4;0,fill=black,o=4
+T5(0,5)->tex=\\scriptsize\\frac{1}{2}/rm/0.5;0,fill=black,o=4
+T6(0,7.07)->tex=\\scriptsize\\frac{\\sqrt{2}}{2}/rm/0.5;0,fill=black,o=4
+T7(0,8.6)->tex=\\scriptsize\\frac{\\sqrt{3}}{2}/rm/0.5;0,fill=black,o=4
+T9(0,10)->tex=\\scriptsize 1/rt/0.5;0,fill=black,o=4
+T11(-5,0)->tex=\\scriptsize-\\frac{1}{2}/cb/0;-0.5,fill=black,o=4
+T12(-7.07,0)->tex=\\scriptsize-\\frac{\\sqrt{2}}{2}/cb/0;-0.5,fill=black,o=4
+T13(-8.6,0)->tex=\\scriptsize-\\frac{\\sqrt{3}}{2}/cb/0;-0.5,fill=black,o=4
+T14(-10,0)->tex=\\scriptsize -1/lb/-0.4;0,fill=black,o=4
+T15(0,-5)->tex=\\scriptsize-\\frac{1}{2}/rm/0.5;0,fill=black,o=4
+T16(0,-7.07)->tex=\\scriptsize-\\frac{\\sqrt{2}}{2}/rm/0.5;0,fill=black,o=4
+T17(0,-8.6)->tex=\\scriptsize-\\frac{\\sqrt{3}}{2}/rm/0.5;0,fill=black,o=4
+T19(0,-10)->tex=\\scriptsize -1/rb/0.5;0,fill=black,o=4
+T20(10,10)->hide
+X1(10,5.77)->tex=\\scriptsize \\frac{\\sqrt{3}}{3}/mr/0.5;0,fill=black,o=4
+X2(10,10)->tex=\\scriptsize 1/mr/0.5;0,fill=black,o=4
+X3(10,17.3)->tex=\\scriptsize \\sqrt{3}/mr/0.5;0,fill=black,o=4
+X4(10,-5.77)->tex=\\scriptsize -\\frac{\\sqrt{3}}{3}/mr/0.5;0,fill=black,o=4
+X5(10,-10)->tex=\\scriptsize -1/mr/0.5;0,fill=black,o=4
+X6(10,-17.3)->tex=\\scriptsize -\\sqrt{3}/mr/0.5;0,fill=black,o=4
+t=T20P0
+@end:static
+P(7.07,7.07)->drag=c
+C=proj P,Ox->hide
+S=proj P,Oy->hide
+c=PC.->dash
+s=PS.->dash
+p0=PA->dot
+p=PA.->w=3
+c1=AC.->green,w=5
+s1=AS.->red,w=5
+T=inter p,t
+t1=P0T.->gold,w=5
+a=arc P0,A,P,2->tex=\\theta`,s="axis,x=-12:12,y=-20:20,ppu=20",y=l({__name:"cercle-trigonometrique",setup(o){return(f,p)=>(r(),t("div",a,[c(i,{draw:{code:e,parameters:s}},null,8,["draw"])]))}});export{y as default};
