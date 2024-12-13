1b635120b8acb0a908baa1606c7ce31c88807b1b
Basil Gass <basil.gass@bluewin.ch>
2024-12-13 14:47:02 +0100

    Update ScolcoursLogo.vue pour gérer correctement le scale-150...

--- resources/js/Components/ScolcoursLogo.vue
+++ resources/js/Components/ScolcoursLogo.vue
@@ -5,11 +5,13 @@ Logo du site
 </script>
 <template>
 	<div class="text-center">
-		<div class="text-4xl">
-			{{ $page.props.scolcours.title }}
-		</div>
-		<div class="text-sm">
-			powered by Scolcours
+		<div class="text-center py-10 scale-150 inline-block">
+			<div class="text-4xl">
+				{{ $page.props.scolcours.title }}
+			</div>
+			<div class="text-sm font-extralight">
+				powered by Scolcours
+			</div>
 		</div>
 	</div>
 </template>
--- resources/js/Pages/HomePage.vue
+++ resources/js/Pages/HomePage.vue
@@ -12,7 +12,7 @@ defineOptions({layout: LayoutFullpage})
 	<div class="space-y-10 bg-gray-100">
 		<!--		<Head title="" />-->
 
-		<ScolCoursLogo class="py-10 scale-150" />
+		<ScolCoursLogo />
 
 		<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
 			<InertiaLink
