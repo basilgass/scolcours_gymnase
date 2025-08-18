2a6e1505f63bfbad630d2b0b3e5cbf9a85e27d48
Basil Gass <basil.gass@bluewin.ch>
2025-08-18 17:45:48 +0200

    Build OK

---
+++ public/build/assets/LessonIconLegend-BWzOoLrd.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./LessonIconLegend.vue_vue_type_script_setup_true_lang-BiP-WJ2q.js";import"./LessonTypeIcon.vue_vue_type_script_setup_true_lang-DTOKDnFQ.js";import"./@vue-DuaRNG-c.js";export{o as default};
