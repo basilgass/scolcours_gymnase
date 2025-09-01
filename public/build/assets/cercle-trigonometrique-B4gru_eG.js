2a79a7960bfe80446d8912873e7c89efb48b946d
Basil Gass <basil.gass@bluewin.ch>
2025-09-01 16:29:36 +0200

    Build OK

---
+++ public/build/assets/cercle-trigonometrique-B4gru_eG.js
@@ -0,0 +1,56 @@
+import{_ as i}from"./PiDrawParser.vue_vue_type_script_setup_true_lang-Du41-taE.js";import{_ as r}from"./Card.vue_vue_type_script_setup_true_lang-BDEgDQgF.js";import{d as t,O as l,G as c,P as o,Q as p}from"./@vue-mHX2EraA.js";import"./scButton.vue_vue_type_script_setup_true_lang-DB8w7EVL.js";import"./app-BCmIpFQ-.js";import"./@mermaid-js-BkNdq-V2.js";import"./langium-B3Oy5o8n.js";import"./vscode-jsonrpc-1VOqS1Ai.js";import"./chevrotain-ufdGC5RL.js";import"./@chevrotain-DTdGFJgf.js";import"./lodash-es-MXiO74gM.js";import"./chevrotain-allstar-CQH327D_.js";import"./vscode-languageserver-types-NoPvPymt.js";import"./vscode-languageserver-textdocument-CKBVUiR3.js";import"./vscode-uri-CI4Qs-K4.js";import"./axios-NIGUFBhG.js";import"./@inertiajs-DWYGoxiA.js";import"./qs-BeCYdERL.js";import"./side-channel-0MiGgEhS.js";import"./es-errors-CxTyLFAO.js";import"./object-inspect-C1WA4GUA.js";import"./@braintree-CgcfzsAa.js";import"./side-channel-list-EGvMd5XU.js";import"./side-channel-map-fmgflpBz.js";import"./get-intrinsic-BpLglCTt.js";import"./es-object-atoms-BR76m4z7.js";import"./math-intrinsics-DP1tGiab.js";import"./gopd-C1lCZ5Qs.js";import"./es-define-property-D_7cP-M3.js";import"./has-symbols-BaUvM3gb.js";import"./get-proto-Cd0SCNDH.js";import"./dunder-proto-DSwuxXkl.js";import"./call-bind-apply-helpers-VtLnLD2e.js";import"./function-bind-CHqF18-c.js";import"./hasown-DwiY0sux.js";import"./call-bound-BuGV4nFB.js";import"./side-channel-weakmap-Dxr6lSWt.js";import"./katex-DUaC9SwU.js";import"./laravel-vite-plugin-DEL3ZhID.js";import"./pinia-CfqAVabU.js";import"./vuedraggable-BjlCR7_b.js";import"./sortablejs-D5Oeo24j.js";import"./@vueuse-ige4WCsB.js";import"./dayjs-DZvSGOSq.js";import"./vue-3-slider-component-CaPCPh_H.js";import"./useScriptLoader-CkOQ2cBR.js";import"./pimath-CpviR_n-.js";import"./PiDrawDisplay.vue_vue_type_script_setup_true_lang-BTHxiGeD.js";import"./pidraw-CRouygVR.js";import"./useStoreEditMode-DsfJ9hAd.js";const a=`@begin:static
+A(0,0)
+c=circ A,10
+P0(10,0)
+P1(8.67,5)->tex=\\frac{\\pi}{6}/mr/0.5;0,fill=black,o=4
+P2(7.07,7.07)->tex=\\frac{\\pi}{4}/mr/0.5;0.4,fill=black,o=4
+P3(5,8.67)->tex=\\frac{\\pi}{3}/tr/0.5;0,fill=black,o=4
+P4(0,10)->tex=\\frac{\\pi}{2}/tl/-0.5;0.2,fill=black,o=4
+P7(-8.67,5)->tex=\\frac{5\\pi}{6}/ml/-0.5;0,fill=black,o=4
+P6(-7.07,7.07)->tex=\\frac{3\\pi}{4}/ml/-0.5;0.4,fill=black,o=4
+P5(-5,8.67)->tex=\\frac{2\\pi}{3}/tl/-0.5;0,fill=black,o=4
+P8(-10,0)->tex=\\frac{3\\pi}{2}/tl/-0.5;0.3,fill=black,o=4
+P9(8.67,-5)->tex=\\frac{11\\pi}{6}/mr/0.5;0,fill=black,o=4
+P10(7.07,-7.07)->tex=\\frac{7\\pi}{4}/br/0.5;0,fill=black,o=4
+P311(5,-8.67)->tex=\\frac{5\\pi}{3}/br/0.5;0,fill=black,o=4
+P12(0,-10)->tex=\\frac{3\\pi}{2}/bl/-0.5;-0.3,fill=black,o=4
+P13(-8.67,-5)->tex=\\frac{7\\pi}{6}/ml/-0.5;0,fill=black,o=4
+P14(-7.07,-7.07)->tex=\\frac{5\\pi}{4}/bl/-0.5;0,fill=black,o=4
+P15(-5,-8.67)->tex=\\frac{4\\pi}{3}/bl/-0.5;0,fill=black,o=4
+T1(5,0)->tex=\\scriptsize\\frac{1}{2}/cb/0;-0.5,fill=black,o=4
+T2(7.07,0)->tex=\\scriptsize\\frac{\\sqrt{2}}{2}/cb/0;-0.5,fill=black,o=4
+T3(8.6,0)->tex=\\scriptsize\\frac{\\sqrt{3}}{2}/cb/0;-0.5,fill=black,o=4
+T4(10,0)->tex=\\scriptsize 1/rb/0.4;0,fill=black,o=4
+T5(0,5)->tex=\\scriptsize\\frac{1}{2}/rm/0.5;0,fill=black,o=4
+T6(0,7.07)->tex=\\scriptsize\\frac{\\sqrt{2}}{2}/rm/0.5;0,fill=black,o=4
+T7(0,8.6)->tex=\\scriptsize\\frac{\\sqrt{3}}{2}/rm/0.5;0,fill=black,o=4
+T9(0,10)->tex=\\scriptsize 1/rt/0.5;0,fill=black,o=4
+T11(-5,0)->tex=\\scriptsize-\\frac{1}{2}/cb/0;-0.5,fill=black,o=4
+T12(-7.07,0)->tex=\\scriptsize-\\frac{\\sqrt{2}}{2}/cb/0;-0.5,fill=black,o=4
+T13(-8.6,0)->tex=\\scriptsize-\\frac{\\sqrt{3}}{2}/cb/0;-0.5,fill=black,o=4
+T14(-10,0)->tex=\\scriptsize -1/lb/-0.4;0,fill=black,o=4
+T15(0,-5)->tex=\\scriptsize-\\frac{1}{2}/rm/0.5;0,fill=black,o=4
+T16(0,-7.07)->tex=\\scriptsize-\\frac{\\sqrt{2}}{2}/rm/0.5;0,fill=black,o=4
+T17(0,-8.6)->tex=\\scriptsize-\\frac{\\sqrt{3}}{2}/rm/0.5;0,fill=black,o=4
+T19(0,-10)->tex=\\scriptsize -1/rb/0.5;0,fill=black,o=4
+T20(10,10)->hide
+X1(10,5.77)->tex=\\scriptsize \\frac{\\sqrt{3}}{3}/mr/0.5;0,fill=black,o=4
+X2(10,10)->tex=\\scriptsize 1/mr/0.5;0,fill=black,o=4
+X3(10,17.3)->tex=\\scriptsize \\sqrt{3}/mr/0.5;0,fill=black,o=4
+X4(10,-5.77)->tex=\\scriptsize -\\frac{\\sqrt{3}}{3}/mr/0.5;0,fill=black,o=4
+X5(10,-10)->tex=\\scriptsize -1/mr/0.5;0,fill=black,o=4
+X6(10,-17.3)->tex=\\scriptsize -\\sqrt{3}/mr/0.5;0,fill=black,o=4
+t=T20P0
+@end:static
+P(7.07,7.07)->drag=c
+C=proj P,Ox->hide
+S=proj P,Oy->hide
+c=PC.->dash
+s=PS.->dash
+p0=PA->dot
+p=PA.->w=3
+c1=AC.->green,w=5
+s1=AS.->red,w=5
+T=inter p,t
+t1=P0T.->gold,w=5
+a=arc P0,A,P,2->tex=\\theta`,e="axis,x=-13:13,y=-18:18,ppu=20",ei=t({__name:"cercle-trigonometrique",setup(s){return(m,f)=>(c(),l(r,{class:"max-w-xl mx-auto"},{default:o(()=>[p(i,{draw:{code:a,parameters:e}},null,8,["draw"])]),_:1}))}});export{ei as default};
