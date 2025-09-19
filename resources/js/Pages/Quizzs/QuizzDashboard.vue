71c77b0a2dfe7b78a6da99043d9aedc8a8bd215e
Basil Gass <basil.gass@bluewin.ch>
2025-09-19 13:22:43 +0200

    Réglage UI dashboard

--- resources/js/Pages/Quizzs/QuizzDashboard.vue
+++ resources/js/Pages/Quizzs/QuizzDashboard.vue
@@ -4,7 +4,6 @@ import LayoutMain from "@/Layouts/LayoutMain.vue"
 import {QuizzSessionInterface} from "@/types/modelInterfaces.ts"
 import axios from "axios"
 import Card from "@/Components/Ui/Card.vue"
-import QuestionShow from "@/Components/Questions/QuestionShow.vue"
 import {AxiosResponseModel} from "@/types"
 import ScButton from "@/Components/Ui/scButton.vue"
 
@@ -50,64 +49,62 @@ const toc = computed(() => {
 </script>
 
 <template>
-	<section class="py-10 space-y-3">
-		<div class="grid grid-cols-1 md:grid-cols-2 gap-3 grid-flow-col-dense">
-			<card>
-				<template #header>
-					<h2
-						v-katex.auto="liveQuizz.quizz.title"
-						class="font-semibold text-lg"
-					/>
-					<h3 class="font-code">
-						{{ liveQuizz.current }} - {{ liveQuizz.status }}
-					</h3>
-				</template>
+	<section class="py-10 relative">
+		<div class="fixed w-full bottom-0 h-[8rem] bg-content grid grid-cols-3 items-center">
+			<div
+				v-if="liveQuizz.current>0"
+				class="p-3 cursor-pointer"
+				@click="updateCurrent(liveQuizz.current-1)"
+			>
+				<i class="bi bi-chevron-left" /> précédant
+			</div><div v-else />
+			<sc-button
+				type="primary"
+				:outline="!liveQuizz.showAnswer"
+				@click="updateShowAnswer"
+			>
+				<i
+					class="cursor-pointer bi bi-eye mr-3"
+				/> réponse
+			</sc-button>
+			<div
+				v-if="liveQuizz.current<liveQuizz.questions.length+1"
+				class="text-right p-3 cursor-pointer"
+				@click="updateCurrent(liveQuizz.current+1)"
+			>
+				suivant <i class="bi bi-chevron-right" />
+			</div><div v-else />
+		</div>
+		<card>
+			<template #header>
+				<h2
+					v-katex.auto="liveQuizz.quizz.title"
+					class="font-semibold text-lg"
+				/>
+				<h3 class="font-code">
+					{{ liveQuizz.current }} - {{ liveQuizz.status }}
+				</h3>
+			</template>
 
-				<div class="grid grid-cols-1 gap-2">
+			<div class="grid grid-cols-1 gap-2">
+				<div
+					v-for="(title, index) in toc"
+					:key="`toc-${index}`"
+					class="flex items-center"
+				>
 					<div
-						v-for="(title, index) in toc"
-						:key="`toc-${index}`"
-						class="flex items-center"
-					>
-						<div
-							class="grid grid-cols-2 py-1 cursor-pointer rounded flex-1"
-							:class="{
-								'bg-blue-100 border border-blue-400  px-3 font-semibold': liveQuizz.current === index
-							}"
-							@click="updateCurrent(index)"
-							v-katex.auto="title"
-						/>
-						<div class="pl-3 w-[80px] flex justify-around items-center">
-							templates
-						</div>
+						class="grid grid-cols-2 py-1 cursor-pointer rounded flex-1"
+						:class="{
+							'bg-blue-100 border border-blue-400  px-3 font-semibold': liveQuizz.current === index
+						}"
+						@click="updateCurrent(index)"
+						v-katex.auto="title"
+					/>
+					<div class="pl-3 w-[80px] flex justify-around items-center">
+						templates
 					</div>
 				</div>
-			</card>
-
-			<div
-				v-if="liveQuizz.status==='question'"
-				class="space-y-3 self-start"
-			>
-				<question-show
-					:question="liveQuizz.questions[liveQuizz.current-1]"
-					editor-mode
-					class="max-w-lg mx-auto"
-					block-only
-					auto-answer
-				/>
-
-				<sc-button
-					type="primary"
-					:outline="!liveQuizz.showAnswer"
-					xs
-					class="w-full"
-					@click="updateShowAnswer"
-				>
-					<i
-						class="cursor-pointer bi bi-eye mr-3"
-					/> afficher/cacher la réponse
-				</sc-button>
 			</div>
-		</div>
+		</card>
 	</section>
 </template>
