e2b29c5ddd6f8995839a45165de0769136a64946
Basil Gass <basil.gass@bluewin.ch>
2025-12-16 17:38:19 +0100

    Build OK

---
+++ public/build/assets/useGenerator-BhzdJOEp.js
@@ -0,0 +1,8 @@
+import{L as c}from"./pimath-BDZBoBsj.js";import{P as d}from"./PiMathExt-DaHdDeEl.js";import{x as r}from"./@vue-DrJuslU9.js";function b(s){function a(e){e===void 0&&(e=n());const t=r(e),o=r(s);return{block:{id:0,title:t.title??"",body:o.template.replace("question",t.question).replace("answer","$a"),illustration:null},keyboard:t.keyboard??o.keyboard,answer:""+t.answer,equationControl:null,user:{is_resolved:!1}}}function i(e){if(e<1)return[];const t=[];for(let o=0;o<e;o++){const u=n();t.some(l=>l.question===u.question)||t.push(n())}return t}function n(){const e=r(s),t=new Function("PiMath","PiMathExt",e.code);try{const o=t(c,d);return o.keyboard||(o.keyboard=e.keyboard),o}catch(o){throw console.warn(o),new Error("Erreur dans la génération de la question (voir la console pour plus détails)")}}return{code:r(s).code??`return {
+	question: "erreur dans la génération de question",
+	answer: "-",
+	keyboard: {
+		name: "",
+		parameters: "exact"
+	}
+}`,question:e=>a(e),list:i,random:()=>n()}}export{b as u};
