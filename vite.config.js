91fc302a5d2bd4f31a47b7229ff967d29d9e44ff
Basil Gass <basil.gass@bluewin.ch>
2026-01-19 18:09:29 +0100

    package bump

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import {defineConfig} from "vite"
 import laravel from "laravel-vite-plugin"
 import vue from "@vitejs/plugin-vue"
 import tailwindcss from "@tailwindcss/vite"
-import path from "path"
 
 export default defineConfig({
 	build: {
